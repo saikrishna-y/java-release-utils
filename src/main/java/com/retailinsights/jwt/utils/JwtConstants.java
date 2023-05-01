ff41f7f925595f327ffdb0b0a8257cc9f519d7d8
sreekar <sreekar.hk@una-brands.com>
2023-05-01 19:10:54 +0530

    initial commit

---
+++ src/main/java/com/retailinsights/jwt/utils/JwtConstants.java
@@ -0,0 +1,14 @@
+package com.retailinsights.jwt.utils;
+
+public class JwtConstants {
+
+    public static final String ROLE_KEY = "role";
+    public static final String PERMISSIONS_KEY = "permissions";
+
+    public static final String AXIS_ROTATION_TYPE = "rotationType";
+
+    public static final String AXIS = "Axis";
+
+    public static final String BEARER_KEY = "Bearer ";
+
+}
